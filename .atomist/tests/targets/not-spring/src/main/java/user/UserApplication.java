37f847fbb6a84b75272f7d5cd7b29f7151628aae
Rod Johnson <rod@atomist.com>
2016-10-08 17:39:25 +1100

    Added new test project

---
+++ .atomist/tests/targets/not-spring/src/main/java/user/UserApplication.java
@@ -0,0 +1,10 @@
+
+
+package user;
+
+public class UserApplication {
+
+    public static void main(String[] args) {
+        System.out.println("This is Java, but it's not a Spring Boot application")
+    }
+}
